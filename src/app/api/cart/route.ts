71303cc762de4b09e70ac0c2c46f8f132bfa3f4b
mhassan000 <hassanasif946@gmail.com>
2023-06-20 15:19:07 +0500

    Create cart route.ts

---
+++ src/app/api/cart/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest,NextResponse } from "next/server";
+import { cartTable, db } from "@/lib/drizzle";
+import {v4} from 'uuid'
+import { cookies } from "next/headers";
+export const GET  =  async (request: NextRequest)=>{
+    try{
+        const res = await db.select().from(cartTable)
+        return NextResponse.json({res})
+
+    }catch(error){
+        return NextResponse.json('Something went wrong')
+
+    }
+}
+
+export const POST  =  async (request: NextRequest)=>{
+
+    const req = await request.json()
+
+    const uid = v4()
+    const setCookies = cookies()
+    const user_id = cookies().get('user_id')?.value
+    
+    if((!user_id)){
+        setCookies.set('user_id',uid)
+    }
+    try{
+        const res = await db.insert(cartTable).values({
+            product_id: req.product_id,
+            Quantity: 1,
+            user_id: user_id as string,
+        }).returning();
+        return NextResponse.json({res})
+
+    }catch(error){
+        console.log ((error as {message:string}).message);
+        return NextResponse.json('Something went wrong')
+
+    }
+}
