b9966cfb1a2335707661662566f62653c1aeee9b
mhassan000 <hassanasif946@gmail.com>
2023-07-06 18:34:45 +0500

    Updated

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -5,5 +5,5 @@ export const client = createClient({
     useCdn: false,
     dataset: 'production',
     projectId: 'jvk18f8f',
-    token: process.env.SANITY_ACCESS_TOKEN
+    token: 'sk1AeSI4xcRff4Xjn6ZYfKoy81JufCeGdQPPk7JRo38MAVoz5Jp6giN6X3W0ioKZmChV2WV10NQm1Yh6uljxoUQzuW0sL6zmdsF2nHzrOae8WkrqJW2YSX4sjuS1SM6D2lmNWkFzIEiCeE9HuDgiFcJ6FimGU6si9vAs0XSf192XcJfref3C'
 })
