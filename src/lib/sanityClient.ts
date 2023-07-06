6d11fd29a2d59f0435a6f3f7c689173568266d7d
mhassan000 <hassanasif946@gmail.com>
2023-07-06 17:55:57 +0500

    Updated cdn

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 export const client = createClient({
     apiVersion: '2023-06-02',
-    useCdn: false,
+    useCdn: true,
     dataset: 'production',
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     token: process.env.SANITY_ACCESS_TOKEN
