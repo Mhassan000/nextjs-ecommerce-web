f1eef1f683660216aba2d040e4126a597f1ebcd8
mhassan000 <hassanasif946@gmail.com>
2023-07-07 04:51:16 +0500

    Updated SanityClient

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,9 +1,9 @@
-import { createClient } from "next-sanity";
+const {createClient} = require('@sanity/client')
 
 export const client = createClient({
     apiVersion: '2023-06-02',
     useCdn: false,
-    dataset: 'production',
-    projectId: 'jvk18f8f',
-    token: 'sk1AeSI4xcRff4Xjn6ZYfKoy81JufCeGdQPPk7JRo38MAVoz5Jp6giN6X3W0ioKZmChV2WV10NQm1Yh6uljxoUQzuW0sL6zmdsF2nHzrOae8WkrqJW2YSX4sjuS1SM6D2lmNWkFzIEiCeE9HuDgiFcJ6FimGU6si9vAs0XSf192XcJfref3C'
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    token: process.env.NEXT_PUBLIC_SANITY_ACCESS_TOKEN
 })
