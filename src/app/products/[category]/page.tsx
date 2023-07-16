ce95c52bbd27f8e930166b7cb9e44f3b0b8b242c
mhassan000 <hassanasif946@gmail.com>
2023-07-16 19:27:06 +0500

    Created categories page

---
+++ src/app/products/[category]/page.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { client } from '@/lib/sanityClient'
+import { Image as IImage, Slug } from "sanity";
+import ProductCard from '@/components/ProductCard';
+ 
+const getProducts  = async (category:string)=>{
+  const res = await client.fetch (`*[_type == "product" &&  category->name == "${category}"]{
+    slug{
+      current
+    },
+    name,
+    ptype,
+    _id,
+    image,
+    price
+  }`);
+  return res
+}
+
+export interface IFProduct {
+  name: string
+  ptype: string
+  image: IImage
+  price: number
+  _id: string
+  slug: Slug
+}
+
+export default async function Page({ params }: { params: { category: string } }) {
+  const {category} = params
+  console.log('Category:',category);
+  const data:IFProduct[] = await getProducts(category)
+  
+  return (
+    <div className='max-w-[92rem] mx-auto  grid px-8 py-8 md:px-20 lg:px-28 sm:grid-cols-2 lg:grid-cols-3 sm:justify-between justify-center items-center xl:grid-cols-4 gap-10'>
+
+      {data.map((item,index)=>{
+        return (
+          <ProductCard item={item} key={index} />
+        )
+      })}
+    
+    </div>
+  )
+}
+
