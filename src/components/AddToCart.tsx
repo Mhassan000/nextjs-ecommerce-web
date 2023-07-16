950ca94e75397c029f8f74d8500b751f061d5b28
mhassan000 <hassanasif946@gmail.com>
2023-07-16 19:28:13 +0500

    Created AddtoCart

---
+++ src/components/AddToCart.tsx
@@ -0,0 +1,25 @@
+'use client'
+import React from 'react'
+import { BiCart } from 'react-icons/bi';
+
+const AddToCart =  ({productId} :{productId:string}) => {
+
+    const handleAddtoCart = async()=>{
+        const res = await fetch ('/api/cart' ,{
+          method: 'POST',
+          body: JSON.stringify({
+            product_id: productId
+          })
+        })     
+        const result = await res.json()
+        console.log('result: ',result)
+    }
+  return (
+    <button onClick={handleAddtoCart} className='bg-black flex items-center text-sm  gap-1 py-2 px-6 text-white border-slate-500 border-t-2 border-l-2 font-semibold'>
+        <span><BiCart fontSize={25}/></span>
+        Add to Cart
+    </button>
+  )
+}
+
+export default AddToCart
