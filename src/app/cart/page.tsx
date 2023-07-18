61a7f04717e1bf0d2d110c12275558ddc4f59ddf
mhassan000 <hassanasif946@gmail.com>
2023-07-18 17:32:54 +0500

    Created Cart Page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='px-8  py-8 mx-auto  md:max-w-[92rem]  md:px-20  xl:px-28'>
+        Shopping Cart
+    </div>
+  )
+}
+
+export default page
