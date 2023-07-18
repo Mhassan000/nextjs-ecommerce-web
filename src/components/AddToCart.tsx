1a4b975795212ee0ddfcbf80e465862720a2ce46
mhassan000 <hassanasif946@gmail.com>
2023-07-18 17:37:07 +0500

    Added add to cart functionality

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,10 +1,8 @@
 'use client'
-import React from 'react'
 import { BiCart } from 'react-icons/bi';
 
 const AddToCart =  ({productId} :{productId:string}) => {
-
-    const handleAddtoCart = async()=>{
+    const handleAddtoCart = async ()=>{
         const res = await fetch ('/api/cart' ,{
           method: 'POST',
           body: JSON.stringify({
@@ -14,11 +12,15 @@ const AddToCart =  ({productId} :{productId:string}) => {
         const result = await res.json()
         console.log('result: ',result)
     }
+
+    
+
   return (
     <button onClick={handleAddtoCart} className='bg-black flex items-center text-sm  gap-1 py-2 px-6 text-white border-slate-500 border-t-2 border-l-2 font-semibold'>
         <span><BiCart fontSize={25}/></span>
         Add to Cart
     </button>
+    
   )
 }
 
