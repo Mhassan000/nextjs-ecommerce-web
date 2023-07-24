8faf3ad72c56a5dc285b17f1fa9f4a434ee8d1b5
mhassan000 <hassanasif946@gmail.com>
2023-07-24 17:54:09 +0500

    Updated Add to Cart Component

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,12 +1,23 @@
 'use client'
+import { useState } from 'react';
 import { BiCart } from 'react-icons/bi';
 
-const AddToCart =  ({productId} :{productId:string}) => {
+const AddToCart =  ({productId ,price} :{productId:string, price:number}) => {
+
+  const [quantity, setQuantity] = useState(1)
+    const handleIncrement =  () => {
+        setQuantity(quantity + 1)
+      }
+      const handleDecrement =  () => {
+        setQuantity((quantity> 1 ? quantity - 1 : 1))
+    }
+
     const handleAddtoCart = async ()=>{
         const res = await fetch ('/api/cart' ,{
           method: 'POST',
           body: JSON.stringify({
-            product_id: productId
+            product_id: productId,
+            Quantity: quantity
           })
         })     
         const result = await res.json()
@@ -16,11 +27,29 @@ const AddToCart =  ({productId} :{productId:string}) => {
     
 
   return (
-    <button onClick={handleAddtoCart} className='bg-black flex items-center text-sm  gap-1 py-2 px-6 text-white border-slate-500 border-t-2 border-l-2 font-semibold'>
-        <span><BiCart fontSize={25}/></span>
-        Add to Cart
-    </button>
-    
+    <>
+      <div className='flex items-center gap-4'>
+          <p className='font-semibold'>QUANTITY:</p>
+          <div className='flex items-center gap-4'>
+            <div className='w-8 h-8 bg-[#f1f1f1] border-[#f1f1f1] border rounded-full   shadow-white text-lg cursor-pointer font-semibold  flex justify-center items-center'
+            onClick={handleDecrement}
+            >-</div>
+            <div className=' text-[#666] font-semibold '>{quantity}</div>
+            <div className='w-8 h-8 border border-black rounded-full  text-lg cursor-pointer font-semibold  flex justify-center items-center'
+            onClick={handleIncrement}
+            >+</div>
+          </div>
+      </div>
+
+      <div className='flex items-center gap-5 '>
+                
+        <button onClick={handleAddtoCart} className='bg-black flex items-center text-sm  gap-1 py-2 px-6 text-white border-slate-500 border-t-2 border-l-2 font-semibold'>
+            <span><BiCart fontSize={25}/></span>
+            Add to Cart
+        </button>
+        <p className='text-2xl font-semibold'>${price * quantity}.00</p>
+      </div>
+    </>
   )
 }
 
