fc7d5b22ffc986cd2a476cd9ff6845bd3fdd20dc
mhassan000 <hassanasif946@gmail.com>
2023-07-25 03:09:41 +0500

    Updated Cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,55 +1,76 @@
-'use client'
-import { useState, useEffect } from 'react';
 import Image from 'next/image'
 import React from 'react'
+import { client } from '@/lib/sanityClient'
+
+import { cookies } from 'next/headers';
+import { IFProduct } from '../../../types';
 
 export interface ICProduct {
     
     product_id: string
     Quantity: number
-  }
+}
 
 
 const getCartItems =  async ()=>{
-  const res = await fetch ('http://localhost:3000/api/cart' ,{
+  const user_id = cookies().get('user_id')?.value
+  const res = await fetch (`http://localhost:3000/api/cart?user_id=${user_id}` ,{
     method: 'GET',
   })
   const result = await res.json()
   return result
-}
+  }
 
 
-const Cart =  () => {
-  // const data  =  await getCartItems()
-  const [cart, setCart] = useState([]);
 
-  const fetchCartItems = async () => {
-    const cartItems = await getCartItems();
-    setCart(cartItems.res);
+
+
+const Cart = async  () => {
+  
+  // console.log('data',data)
+  
+  const getProductItems = async ()=>{
+
+    try{
+      const cartItems:ICProduct[]  =  await getCartItems()
+      const productItems:IFProduct[] =  [];
+
+      for ( const cartItem of cartItems ){
+        const res = await client.fetch(`*[_type == "product" && _id == '${cartItem.product_id}' ] {
+          name,
+          ptype,
+          _id,
+          image,
+          price  
+        }`)
+        if( res.length > 0 ){
+          productItems.push(res[0])
+        }
+
+        }
+        return productItems
+      }
     
-  };
-
+    catch(err){
+      console.log('Error fetching product items',err)
+      return []
+    }
+  }
 
   
-  useEffect(() => {
-
-    fetchCartItems();
-    
-  }, []);
-  // if (cart.length == 0) {
-  //   return <div>Loading...</div>;
-  // }
   
-  console.log( 'cart',cart)
-
+  const productItems:IFProduct[] = await getProductItems()
+  console.log('productItems',productItems)
   return (
     
     <div className='px-8  py-8 mx-auto  md:max-w-[92rem]  md:px-20  xl:px-28'>
         <h1>Shopping Cart</h1>
-        {cart.map((item:ICProduct,index)=>(
+        {productItems.map((item,index)=>(
           <div key={index} className='flex flex-col'>
-            <p>{item.Quantity}</p>
-            <p>{item.product_id}</p>
+           
+            <p>{item.name}</p>
+            <p>{item.price}</p>
+            <p>{item.ptype}</p>
             
           </div>
         ))}
