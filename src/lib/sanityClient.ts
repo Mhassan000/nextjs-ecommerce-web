57aaf4df80404d6ae8b1563de248828f00dc42d3
mhassan000 <hassanasif946@gmail.com>
2023-07-26 20:38:58 +0500

    Updated

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -2,7 +2,7 @@ const {createClient} = require('@sanity/client')
 
 export const client = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: 'production',
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
     token: process.env.NEXT_PUBLIC_SANITY_ACCESS_TOKEN,
     apiVersion: '2023-06-02',
     useCdn: false
