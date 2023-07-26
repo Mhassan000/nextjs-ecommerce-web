11e4038575253f0e3b47f76f7943ac32b186cec9
mhassan000 <hassanasif946@gmail.com>
2023-07-26 18:01:25 +0500

    Remove sanityClient.ts from repository

--- src/lib/sanityClient.ts
+++
@@ -1,9 +0,0 @@
-const {createClient} = require('@sanity/client')
-
-export const client = createClient({
-    projectId: 'jvk18f8f',
-    dataset: 'production',
-    token: process.env.NEXT_PUBLIC_SANITY_ACCESS_TOKEN,
-    apiVersion: '2023-06-02',
-    useCdn: false
-})
