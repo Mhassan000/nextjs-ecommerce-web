0a39d574f374a373963d57a8eb59f414aefc63b3
mhassan000 <hassanasif946@gmail.com>
2023-08-18 17:43:00 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,13 @@
 import Banner from "@/components/Banner";
-import ProductCart from "@/components/ProductCart";
-import { client } from "@/lib/sanityClient"
-import { Image as IImage } from "sanity";
-import Image from "next/image";
 import Events from "@/components/Events";
 import Newsletter from "@/components/Newsletter";
 import FeatureBanner from "@/components/FeatureBanner";
-const getProducts = async ()=>{
-  const res = await client.fetch(`*[_type == 'product']{
-    name,
-    description,
-    _id,
-    image,
-    
-  }`);
-  return res
-}
-
-export interface IProduct {
-  name: string,
-  description: string
-  image: IImage
-  _id: string
-}
 
 export default async function Home() {
-  const data:IProduct[] = await getProducts()
-  
-  
+ 
   return (
     <>
-    
     <Banner />
-    {/* <div className="flex flex-wrap h-screen items-center gap-3 justify-center ">
-    {data.map((item,index)=>(
-      <div key={index}>
-        <ProductCart item = {item} />
-      </div>
-    ))}
-      
-
-   </div>
-    */}
     <Events />
     <FeatureBanner />
     <Newsletter />
