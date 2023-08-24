6df111a0a840d57fde4ca235c0f365d49055d498
mhassan000 <hassanasif946@gmail.com>
2023-08-25 04:16:23 +0500

    Update cart api

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest,NextResponse } from "next/server";
 import { cartTable, db } from "@/lib/drizzle";
-import {v4} from 'uuid'
-import { cookies } from "next/headers";
 import { eq } from "drizzle-orm";
 
 export const GET  =  async (request: NextRequest)=>{
@@ -19,19 +17,11 @@ export const GET  =  async (request: NextRequest)=>{
 export const POST  =  async (request: NextRequest)=>{
 
     const req = await request.json()
-
-    const uid = v4()
-    const setCookies = cookies()
-    const user_id = cookies().get('user_id')?.value
-    
-    if((!user_id)){
-        setCookies.set('user_id',uid)
-    }
     try{
         const res = await db.insert(cartTable).values({
             product_id: req.product_id,
             Quantity: req.Quantity,
-            user_id: user_id || uid as string,
+            user_id: req.user_id,
         }).returning();
         return NextResponse.json({res})
 
