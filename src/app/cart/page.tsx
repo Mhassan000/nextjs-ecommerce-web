bf20996ec02f67d4fd56fecf1ccfd120c46c81c9
mhassan000 <hassanasif946@gmail.com>
2023-08-27 18:45:39 +0500

    Update Cart Page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,92 +1,140 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { client } from '@/lib/sanityClient'
 import { urlForImage } from '../../../sanity/lib/image';
-import { cookies } from 'next/headers';
 import { IFProduct } from '../../../types';
+import {AiOutlineShopping} from 'react-icons/ai'
+import { useAuth } from '@clerk/nextjs';
 
 export interface ICProduct {
-    
     product_id: string
     Quantity: number
 }
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
-const getCartItems =  async ()=>{
-  const user_id = cookies().get('user_id')?.value
-  const res = await fetch (`${baseUrl}/api/cart?user_id=${user_id}` ,{
-    cache:'no-store'
-  })
-  const result = await res.json()
-  return result
+const GetCartItems =  async (userId: string | null | undefined)=>{
+  console.log('---User ID:',userId)
+  try{
+    const res = await fetch (`${baseUrl}/api/cart?user_id=${userId}`,{
+      method: 'GET',
+    })
+    const result = await res.json()
+    return result
+  }
+  catch(err){
+    console.log('Error fetching cart items',err)
+  }
   }
 
+  
 
+const getProductItems = async (userId: string | null | undefined)=>{
 
-  const getProductItems = async ()=>{
+  try{
+    const cartItems:ICProduct[]  =  await GetCartItems(userId)
+    const productItems: ( IFProduct & { quantity: number})[] =  [];
 
-    try{
-      const cartItems:ICProduct[]  =  await getCartItems()
-      const productItems:IFProduct[] =  [];
+    for ( const cartItem of cartItems ){
+      const res = await client.fetch(`*[_type == "product" && _id == '${cartItem.product_id}' ] {
+        name,
+        ptype,
+        _id,
+        image,
+        price  
+      }`)
 
-      for ( const cartItem of cartItems ){
-        const res = await client.fetch(`*[_type == "product" && _id == '${cartItem.product_id}' ] {
-          name,
-          ptype,
-          _id,
-          image,
-          price  
-        }`)
-        if( res.length > 0 ){
-          productItems.push(res[0])
+      if (res.length > 0) {
+        const existingProductIndex = productItems.findIndex(item => item._id === cartItem.product_id);
+        
+        if (existingProductIndex !== -1) {
+          // Product already exists in the cart, update the quantity
+          productItems[existingProductIndex].quantity += cartItem.Quantity;
+        } else {
+          // Product doesn't exist in the cart, add it
+          productItems.push({
+            ...res[0],
+            quantity: cartItem.Quantity,
+          });
         }
-
-        }
-        return productItems
       }
-    
-    catch(err){
-      console.log('Error fetching product items',err)
-      return []
+
+      }
+      return productItems
     }
+  
+  catch(err){
+    console.log('Error fetching product items',err)
+    return []
+  }
   }
 
 
-const Cart = async  () => {
-  const productItems:IFProduct[] = await getProductItems()
-  console.log('productItems',productItems)
+  const Cart = () => {
+    const {userId} = useAuth()
+    // console.log(typeof userId)
+    const [productItems, setProductItems] = useState< (IFProduct & {quantity: number})[]>([]);
 
-  return (
+    useEffect(() => {
+      async function fetchProductItems() {
+        const items: (IFProduct & {quantity: number})[] = await getProductItems(userId);
+        setProductItems(items);
+      }
+  
+      fetchProductItems();
+    }, [userId]);
+    console.log('productItems',productItems)
+
+
+    // User not logged in
+    if(!userId) return (
+      <div className='px-8 py-8 mx-auto md:max-w-[92rem] md:px-20 xl:px-32'>
+        <h1 className='text-[black] font-bold text-2xl'>Shopping Cart</h1>
+        <div className='flex items-center  flex-col gap-y-1 mt-10'>
+          <AiOutlineShopping fontSize={150} />
+          <h1 className='text-[black] font-bold text-2xl'>Please login to view your cart</h1>
+        </div>
+      </div>
+    )
+  
     
-    <div className='px-8  py-8 mx-auto  md:max-w-[92rem]  md:px-20  xl:px-28'>
+    return (
+      <div className='px-8 py-8 mx-auto md:max-w-[92rem] md:px-20 xl:px-32'>
         <h1 className='text-[black] font-bold text-2xl'>Shopping Cart</h1>
         <div>
-
+          {productItems.length === 0 ? (
+            <div className='flex items-center  flex-col gap-y-1 mt-10'>
+              <AiOutlineShopping fontSize={150} />
+              <h1 className='text-[black] font-bold text-2xl'>Your shopping bag is empty</h1>
+            </div>
+          ) : (
+            productItems.map((item, index) => (
+              <div key={index} className='flex flex-col sm:flex-row gap-x-8'>
+                <div>
+                  <Image
+                    className='rounded-xl object-contain '
+                    src={urlForImage(item.image[0] as any) as string}
+                    alt=''
+                    width={180}
+                    height={180}
+                  />
+                </div>
+                <div className='flex flex-col font-semibold gap-y-5'>
+                  <h2 className='text-[#212121] font-medium'>{item.name}</h2>
+                  <p className='text-[#666]'>{item.ptype}</p>
+                  <p className='text-[#212121]'>Delivery Estimation</p>
+                  <p className='text-[#ffc700]'>5 Working Days</p>
+                  <p className='text-lg'>${item.price}</p>
+                  <p className='text-[#666]'>Quantity: {item.quantity}</p>
+                </div>
+              </div>
+            ))
+          )}
         </div>
-        {productItems.map((item,index)=>(
-          <div key={index} className='flex gap-x-2'>
-           
-            <div >
-              <Image className='rounded-xl'
-              src={urlForImage(item.image[0] as any) as string}
-              alt=""
-              width={200}
-              height={200}
-              />
-            </div>
-            <div className='flex flex-col gap-y-2'>
-              <h1>{item.name}</h1>
-              <p>{item.ptype}</p>
-              <p>Delivery Estimation</p>
-              <p>5 Working Days</p>
-              <p>${item.price}</p>
-            </div>
-          </div>
-        ))}
-
-    </div>
-  )
-}
-
-export default Cart
+      </div>
+    );
+  };
+  
+  export default Cart;
+  
